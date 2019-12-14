a77ab0f0832debbf2de4711dd435d037bcac096d
ihanseol <hansel33@naver.com>
2019-12-14 15:51:36 +0900

    add cmder config.file

---
+++ config/profile.d/user_profile.bat
@@ -0,0 +1,2 @@
+@echo off
+cd c:\users\minhwasoo
---
+++ config/user_aliases.cmd
@@ -0,0 +1,26 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+gs=git status $*
+ls=ls --show-control-chars -F --color $*
+ll=ls -l -F --show-control-chars --color $*
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+lp=cls $t dir
+eh=%VI% "C:\windows\system32\drivers\etc\hosts"
+qal=%VI% "%CMDER_ROOT%\config\user_aliases.cmd" $t alias/reload
+sett=%VI% "%CMDER_ROOT%\config\user_profile.cmd"
+min=c: $t cd %ME%
+ana=cd C:\ProgramData\Anaconda3\envs
+sub=sublime_text $*
+
