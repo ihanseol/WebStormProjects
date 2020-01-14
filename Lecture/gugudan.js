c62228f27a320c2ea06425020e9a0b96061f21da
ihanseol <hanseol33@naver.com>
2020-01-14 16:40:26 +0900

    gugudan ts

--- Lecture/gugudan.js
+++ Lecture/gugudan.js
@@ -1,2 +1,8 @@
-var a = 1;
-console.log(' hello TypeScript ', a);
+var numberOne = Math.ceil(Math.random() * 9);
+var numberTwo = Math.ceil(Math.random() * 9);
+var result = numberOne * numberTwo;
+var word = document.createElement('div');
+// word.textContent = ` ${numberOne} * ${numberTwo} = ${result}`;
+word.textContent = '  ' + String(numberOne) + ' multiply ' + String(numberTwo) + ' = ? ';
+document.body.append(word);
+console.log(' hello type script ....');
--- Lecture/gugudan.ts
+++ Lecture/gugudan.ts
@@ -1,8 +1,16 @@
+let numberOne = Math.ceil(Math.random() * 9);
+let numberTwo = Math.ceil(Math.random() * 9);
 
 
-const a : number = 1;
+let result = numberOne * numberTwo;
 
-console.log(' hello TypeScript ', a);
+const word = document.createElement('div');
+// word.textContent = ` ${numberOne} * ${numberTwo} = ${result}`;
+word.textContent = '  ' + String(numberOne) + ' multiply ' + String(numberTwo) + ' = ? ';
 
+
+document.body.append(word);
+
+console.log(' hello type script ....');
 
 
